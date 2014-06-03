8c8ab837e94fc8b74057839af8b7dd70eb32cae4
Annie Kramer <akramer20@gmail.com>
2014-06-02 20:54:09 -0700

    Adjusted formatting

--- hailstonesolution.py
+++ hailstonesolution.py
@@ -1,14 +1,16 @@
 # Hailstone code below.
-def buggle(n):
+def hailstone(n):
     count = 0
+    print n, "is the starting number."
     while n != 1:
-        print n,
         if n % 2 == 0:
             n = n / 2
         else:
             n = (n*3) + 1
         count = count + 1
-    print n
+        print n
     print "It took" , count , "steps to get to 1."
 
-buggle(36)
+hailstone(36)
+hailstone (37)
+hailstone (39)
